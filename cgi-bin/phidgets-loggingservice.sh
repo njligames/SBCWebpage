5a5955766f1f476c91369cd4e3984020f7b16ba8
NJLIGAMES <jamesfolk1@gmail.com>
2016-07-07 14:13:50 +0000

    added a 'logging' tab to the configuration webpage

---
+++ cgi-bin/phidgets-loggingservice.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/haserl
+<? 
+. /usr/lib/webif/webif.sh
+
+load_settings network
+load_settings phidgetwebservice
+
+hostname=$( hostname )
+
+if empty "$FORM_submit"; then
+	FORM_enabled="$pws_enabled"
+	FORM_serverid="$pws_serverid"
+	FORM_pass="$pws_password"
+	FORM_port="$pws_port"
+else
+
+	if equal "$FORM_startstop" "Start"; then
+		/etc/init.d/phidgetwebservice forcestart
+		sleep 1
+		
+	elif equal "$FORM_startstop" "Stop"; then
+		/etc/init.d/phidgetwebservice stop
+		sleep 1
+	else
+		validate <<EOF
+string|pws_serverid|Server ID|min=1 max=63 cfg|$FORM_serverid
+int|FORM_port|Port|required min=1 max=65535 cfg|$FORM_port
+password|pws_password|Password|min=1 max=63 cfg|$FORM_pass
+EOF
+
+		equal "$?" 0 && {
+			save_setting phidgetwebservice pws_password "$FORM_pass"
+			save_setting phidgetwebservice pws_enabled "$FORM_enabled"
+			save_setting phidgetwebservice pws_port "$FORM_port"
+			if [ "$FORM_serverid" != "$hostname" ]; then
+				save_setting phidgetwebservice pws_serverid "$FORM_serverid"
+			else
+				save_setting phidgetwebservice pws_serverid ""
+				FORM_serverid=
+			fi
+			apply_saves
+		}
+	fi
+fi
+
+header "Phidgets" "Logging" "Phidget Logging" "$SCRIPT_NAME"
+
+?>
+<script type="text/javascript">
+/* <![CDATA[ */
+function clearServerID()
+{
+  if(document.getElementById("serverid").value == "<? echo -n "$hostname" ?>")
+  {
+    document.getElementById("serverid").style.color = "black"
+    document.getElementById("serverid").value = ""
+  }
+}
+
+function resetServerID()
+{
+  if(document.getElementById("serverid").value == "")
+  {
+    document.getElementById("serverid").value = "<? echo -n "$hostname" ?>"
+    document.getElementById("serverid").style.color = "gray"
+  }
+}
+/* ]]> */
+</script>
+<?
+
+if [ -z "$FORM_serverid" ]; then
+	serverid_form="<input id=\"serverid\" type=\"text\" name=\"serverid\" value=\"$hostname\" style=\"color:gray\" onfocus=\"clearServerID()\" onclick=\"clearServerID()\" onblur=\"resetServerID()\"/>"
+else
+	serverid_form="<input id=\"serverid\" type=\"text\" name=\"serverid\" value=\"$FORM_serverid\" onfocus=\"clearServerID()\" onclick=\"clearServerID()\" onblur=\"resetServerID()\"/>"
+fi
+
+
+running="Stopped"
+ps -Afww | grep -v grep | grep -q "phidgetwebservice21" && running="Running"
+
+if equal "$running" "Stopped"; then
+	startstop="Logging is <span style=\"color:red\">stopped</span>... <input type=\"submit\" value=\"Start\" name=\"startstop\" />"
+else
+	startstop="Logging is <span style=\"color:green\">running</span>... <input type=\"submit\" value=\"Stop\" name=\"startstop\" />"
+fi
+
+display_form <<EOF
+start_form|Phidget Logging
+field|
+radio|enabled|$FORM_enabled|true|Enabled
+radio|enabled|$FORM_enabled|false|Disabled
+field|<br />
+field|
+string|$startstop
+end_form
+EOF
+
+footer ?>
+
+<!--
+##WEBIF:name:Phidgets:200:Logging
+-->
